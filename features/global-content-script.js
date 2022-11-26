8227e74d23f98bcbf2ecf73cd8a3f83c04ea5c17
ilpersi <43588107+ilpersi@users.noreply.github.com>
2022-11-26 19:30:17 +0100

    Improved documentation for global scripts

--- features/global-content-script.js
+++ features/global-content-script.js
@@ -1,11 +1,18 @@
 'use strict';
 
-const JQ = jQuery.noConflict();
 const globalOptions = {
     'searchable_tables': true,
     'move_to_shortcut': true,
 };
 
+// Let's be sure that there is no JQuery conflict
+const JQ = jQuery.noConflict();
+
+
+/**
+ * Enables the DataTable plugin whenever is possible.
+ *
+ */
 function searchableTables() {
     JQ(document).ready(function () {
         try {
@@ -20,14 +27,18 @@ function searchableTables() {
     });
 }
 
+/**
+ * Takes care of creating all the "Move to locale" links whenever is is possible
+ *
+ */
 function moveToLocaleLink() {
     const LOCALE_A_ELEMM = "//a[contains(@href, 'Locale/')]";
     const LOCALE_LINK_RE = /\/Locale\/(\d+)/gm;
-    
+
     // Let's get the locales a nodes
     let xpathHelper = new XPathHelper(LOCALE_A_ELEMM);
     let localeNodes = xpathHelper.getOrderedSnapshot(document);
-        
+
     for (let i = 0; i < localeNodes.snapshotLength; i++) {
         let aElem = localeNodes.snapshotItem(i);
         LOCALE_LINK_RE.lastIndex = 0;
@@ -35,26 +46,26 @@ function moveToLocaleLink() {
         // We need aditional checks on the location url
         let localeHref = aElem.getAttribute('href');
         let localeMatch = LOCALE_LINK_RE.exec(localeHref);
-        
+
         // XPath is matching, but it is not exactly a locale url
         if (!localeMatch) continue;
 
         // We are looking at a locale page and we do not want to add icon in there, as the move link is available
         if (window.location.href.includes('/Locale/' + localeMatch[1])) continue;
-        
+
         // New a elem
         var moveAElem = document.createElement('a');
         moveAElem.setAttribute('href', window.location.origin + '/World/Popmundo.aspx/Locale/MoveToLocale/' + localeMatch[1]);
         moveAElem.textContent = ' ';
-        
+
         // Move to icon
         let imgElem = document.createElement('img');
         imgElem.setAttribute('src', chrome.runtime.getURL('images/Gnome-application-exit.png'));
         imgElem.setAttribute('title', 'Move To Locale');
-        
+
         // Include incon in link
         moveAElem.appendChild(imgElem);
-        
+
         // Append to popmundo page
         aElem.parentNode.insertBefore(moveAElem, aElem.nextSibling);
     }
