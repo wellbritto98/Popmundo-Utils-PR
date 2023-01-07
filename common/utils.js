ae88af52f972633784fb32880cc4a03323af6420
ilpersi <43588107+ilpersi@users.noreply.github.com>
2023-01-07 18:00:50 +0100

    Documenting the Utils class

--- common/utils.js
+++ common/utils.js
@@ -1,4 +1,16 @@
+/**
+ * A generic class containing misc utilities methods to be used in different content scripts
+ *
+ * @class Utils
+ */
 class Utils {
+    /**
+     * Get the id of your own character.
+     *
+     * @static
+     * @return {int} The id of your character if found, otherwise 0
+     * @memberof Utils
+     */
     static getMyID() {
         let idHolderElem = document.querySelector('div.idHolder');
 
