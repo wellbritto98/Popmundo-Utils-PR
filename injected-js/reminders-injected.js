056401a0999d242350ee429cdf817f731b1cd96c
ilpersi <43588107+ilpersi@users.noreply.github.com>
2023-01-15 11:06:55 +0100

    Documentation for injected scripts

--- injected-js/reminders-injected.js
+++ injected-js/reminders-injected.js
@@ -1,8 +1,20 @@
-function deleteAndredirect(characterID, itemID) {
+/**
+ * Injected function to perform database management and redirect the user to the item page.
+ * https://stackoverflow.com/a/9517879/1280443
+ * https://stackoverflow.com/a/19312198/1280443
+ * 
+ * All the data are passed trough to a custom event handler
+ *
+ * @param {int} characterID The id of currently logged in character
+ * @param {int} itemID The id of the item to delete from the saved storage
+ * @return {boolean} Always return false to block event execution on link. 
+ */
+function deleteAndredirect(characterID, itemID, redirect = true) {
 
     var data = {
         characterID: characterID,
         itemID: itemID,
+        redirect: redirect,
     };
 
     document.dispatchEvent(new CustomEvent('deleteAndredirect', { detail: data }));
