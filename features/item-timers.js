7e9948e2dec6646998e274715b5f3b48dc6c0bd2
ilpersi <43588107+ilpersi@users.noreply.github.com>
2023-12-25 13:04:21 +0100

    Timers rounding logic.

--- features/item-timers.js
+++ features/item-timers.js
@@ -65,6 +65,10 @@ async function checkForTimer() {
             
             // We only update the timer when we find something
             if (minutes > 0 || hours > 0 || days > 0 || weeks > 0) {
+                // we round minutes and hours to avoid any false notification
+                minutes = minutes > 0 ? minutes + 1 : minutes;
+                hours = hours > 0 ? hours + 1 : hours;
+
                 // We add duration to current time
                 let nowTimeStamp = now.getTime();
                 let timerTimeStamp = nowTimeStamp + (weeks * WEEK) + (days * DAY) + (hours * HOUR) + (minutes * MINUTE);
