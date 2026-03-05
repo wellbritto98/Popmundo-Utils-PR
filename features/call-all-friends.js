0647d9e24b2e62cc77d586d7e57ca6d7affe022b
ilpersi <43588107+ilpersi@users.noreply.github.com>
2026-03-05 17:52:46 +0100

    Improve error handling in call all friends.

--- features/call-all-friends.js
+++ features/call-all-friends.js
@@ -201,25 +201,8 @@ async function onSubmitClick() {
                     statusPElem.textContent = `Calling friend ${friendIndex + 1} out of ${callableFriends.length} (${friendDetails.name})...`;
 
                     fetch(friendDetails.interactUrl, {
-                        // "headers": {
-                        //     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-                        //     "accept-language": "en-US,en;q=0.7",
-                        //     "cache-control": "max-age=0",
-                        //     "content-type": "application/x-www-form-urlencoded",
-                        //     "sec-fetch-dest": "document",
-                        //     "sec-fetch-mode": "navigate",
-                        //     "sec-fetch-site": "same-origin",
-                        //     "sec-fetch-user": "?1",
-                        //     "sec-gpc": "1",
-                        //     "upgrade-insecure-requests": "1",
-                        //     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-                        // },
-                        // "referrer": friendDetails.interactUrl,
-                        // "referrerPolicy": "strict-origin-when-cross-origin",
                         "body": friendDetails.formData,
                         "method": "POST",
-                        // "mode": "cors",
-                        // "credentials": "include"
                     }).then((response) => {
                         if (response.ok && response.status >= 200 && response.status < 300) {
                             succesCalls += 1;
@@ -227,6 +210,9 @@ async function onSubmitClick() {
                         }
                     }).then((html) => {
                         resolve(true);
+                    }).catch((error) => {
+                        console.error(`Failed to call ${friendDetails.name}:`, error);
+                        resolve(false);
                     });
 
                 }, CALL_DELAY * friendIndex);
