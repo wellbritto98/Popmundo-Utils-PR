5a68ec4bfafb4bfa85d5ce24c9b779f85c2a74d1
ilpersi <43588107+ilpersi@users.noreply.github.com>
2026-03-05 18:57:10 +0100

    Review autograph setting layout.

--- options/options.js
+++ options/options.js
@@ -28,7 +28,7 @@ const optionDetails = [
     { 'name': 'show_msg_helper', 'default': false, 'save_cb': saveCheckBox, 'load_cb': loadCheckBox },
     { 'name': 'strip_percent_txt', 'default': false, 'save_cb': saveCheckBox, 'load_cb': loadCheckBox },
     { 'name': 'score_scale', 'default': '0_26', 'save_cb': saveSelect, 'load_cb': loadSelect },
-    { 'name': 'autograph_book_name', 'default': '', 'save_cb': saveSelect, 'load_cb': loadSelect },
+    { 'name': 'autograph_book_name', 'default': 'Autograph book', 'save_cb': saveSelect, 'load_cb': loadSelect },
 
     // pop up options
     { 'name': 'character_popup', 'default': true, 'save_cb': saveCheckBox, 'load_cb': loadCheckBox },
